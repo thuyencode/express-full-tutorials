e186c9bad692476a7a39496b3c4cfb87bc4cadaf
Thuyen Code <thuyencode@proton.me>
2024-10-31 19:25:11 +0700

    Express JS #3 - Route Parameters

--- main.ts
+++ main.ts
@@ -1,13 +1,15 @@
 // @deno-types="@types/express"
 import express from 'express'
-import tutorial_2_routes from './tutorial-2/routes.ts'
+import tutorial_2_routes from './tutorial-2/tutorial-2.routes.ts'
+import tutorial_3_routes from './tutorial-3/tutorial-3.routes.ts'
 
 const PORT = Deno.env.get('PORT') || 8080
 
 const app = express()
 
 app.use('/tutorial-2', tutorial_2_routes)
+app.use('/tutorial-3', tutorial_3_routes)
 
 app.listen(PORT, () => {
-  console.log(`Running on port ${PORT}`)
+  console.log(`Running on port ${PORT}.`)
 })
---
+++ tutorial-3/tutorial-3.routes.ts
@@ -0,0 +1,45 @@
+// @deno-types="@types/express"
+import express from 'express'
+
+const MOCKED_USERS = [
+  { id: 1, username: 'hello', name: 'Hello' },
+  { id: 2, username: 'world', name: 'World' },
+] as const
+
+const tutorial_3_routes = express.Router()
+
+tutorial_3_routes.get('/', (_req, res) => {
+  res.status(201).send({ message: 'Hello, World!' })
+})
+
+tutorial_3_routes.get('/api/users', (_req, res) => {
+  res.send(MOCKED_USERS)
+})
+
+tutorial_3_routes.get('/api/products', (_req, res) => {
+  res.send([
+    { id: 1, product: 'deno', version: '2' },
+    { id: 2, product: 'nodejs', name: '23' },
+  ])
+})
+
+// deno-lint-ignore no-explicit-any
+tutorial_3_routes.get('/api/users/:id', (req, res): any => {
+  const parsedId = Number.parseInt(req.params.id)
+
+  if (Number.isNaN(parsedId)) {
+    return res.status(400).send({ error: 'Bad Request. Invalid ID.' })
+  }
+
+  const user = MOCKED_USERS.find(({ id }) => id === parsedId)
+
+  if (!user) {
+    return res.status(404).send({
+      error: `User with id '${parsedId}' not found.`,
+    })
+  }
+
+  res.send(user)
+})
+
+export default tutorial_3_routes
