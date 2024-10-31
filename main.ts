10937bc5ccd9cc8a79a9f02cef035288c42edafa
Thuyen Code <thuyencode@proton.me>
2024-10-31 21:25:46 +0700

    Express JS #4 - Query Parameters

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@
 import express from 'express'
 import tutorial_2_routes from './tutorial-2/tutorial-2.routes.ts'
 import tutorial_3_routes from './tutorial-3/tutorial-3.routes.ts'
+import tutorial_4_routes from './tutorial-4/tutorial-4.routes.ts'
 
 const PORT = Deno.env.get('PORT') || 8080
 
@@ -9,6 +10,7 @@ const app = express()
 
 app.use('/tutorial-2', tutorial_2_routes)
 app.use('/tutorial-3', tutorial_3_routes)
+app.use('/tutorial-4', tutorial_4_routes)
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}.`)
---
+++ tutorial-4/tutorial-4.routes.ts
@@ -0,0 +1,43 @@
+// deno-lint-ignore-file no-explicit-any
+// @deno-types="@types/express"
+import e from 'express'
+
+const MOCKED_USERS = [
+  { id: 1, username: 'javascript', name: 'JavaScript' },
+  { id: 2, username: 'typescript', name: 'TypeScript' },
+  { id: 3, username: 'java', name: 'Java' },
+  { id: 4, username: 'rust', name: 'Rust' },
+  { id: 5, username: 'zig', name: 'Zig' },
+  { id: 6, username: 'gdscript', name: 'GDScript' },
+] as const
+
+const tutorial_4_routes = e.Router()
+
+interface ReqQuery {
+  filter?: keyof typeof MOCKED_USERS[number]
+  value?: string
+}
+
+tutorial_4_routes.get(
+  '/api/users',
+  // deno-lint-ignore ban-types
+  (req: e.Request<{}, {}, {}, ReqQuery>, res): any => {
+    const { filter, value } = req.query
+
+    if (filter && value) {
+      return res.send(MOCKED_USERS.filter((user) => {
+        const result = user[filter]
+
+        if (typeof result === 'string') {
+          return result.toLowerCase().includes(value.toLowerCase())
+        }
+
+        return result === Number(value)
+      }))
+    }
+
+    res.send(MOCKED_USERS)
+  },
+)
+
+export default tutorial_4_routes
