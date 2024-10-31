9908b04e16c114881700b76fe8c610106b66d873
Thuyen Code <thuyencode@proton.me>
2024-10-31 17:02:49 +0700

    Express JS #2 - Get Requests

--- main.ts
+++ main.ts
@@ -1,9 +1,12 @@
-// @deno-types="npm:@types/express"
-import express from 'npm:express'
+// @deno-types="@types/express"
+import express from 'express'
+import tutorial_2_routes from './tutorial-2/routes.ts'
 
 const PORT = Deno.env.get('PORT') || 8080
 
 const app = express()
+
+app.use('/tutorial-2', tutorial_2_routes)
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`)
---
+++ tutorial-2/routes.ts
@@ -0,0 +1,24 @@
+// @deno-types="@types/express"
+import express from 'express'
+
+const tutorial_2_routes = express.Router()
+
+tutorial_2_routes.get('/', (_req, res) => {
+  res.status(201).send({ message: 'Hello, World!' })
+})
+
+tutorial_2_routes.get('/api/users', (_req, res) => {
+  res.send([
+    { id: 1, username: 'hello', name: 'Hello' },
+    { id: 2, username: 'world', name: 'World' },
+  ])
+})
+
+tutorial_2_routes.get('/api/products', (_req, res) => {
+  res.send([
+    { id: 1, product: 'deno', version: '2' },
+    { id: 2, product: 'nodejs', name: '23' },
+  ])
+})
+
+export default tutorial_2_routes
